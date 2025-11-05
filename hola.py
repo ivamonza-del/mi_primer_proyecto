b5e9f9a008c35635ff0401a8055c392d472fbd6e
Ivan <ivamonza@gmail.com>
2025-11-05 18:46:56 -0500

    Primer commit: script de saludo en Python

---
+++ hola.py
@@ -0,0 +1,5 @@
+# hola.py
+# Autor: (Ivan Salomon Montenegro Zambrano)
+# Descripción: Script básico que saluda al usuario
+nombre = input("¿Cómo te llamas? ")
+print(f"¡Hola, {nombre}! Bienvenido a tu primer proyecto con Git y Python.")
